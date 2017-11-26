da4367dcf68e270983f8884b8682b5956928f2d7
Artem <artemradzivon@gmail.com>
2017-11-26 14:05:07 +0200

    Refactoring routes

--- app/routes/vegetablesRoutes.js
+++ app/routes/vegetablesRoutes.js
@@ -1,6 +1,18 @@
-module.exports = function(app, db) {
+const express = require('express'),
+	  router = express.Router(),
+	  bodyParser = require('body-parser'),
+	  parseUrlencoded = bodyParser.urlencoded({extended: false});
+let db = {};
 
-  	app.get('/vegetables/:name', (req, res) => {
+router.use((req, res, next) => {
+	db = req.app.get('db');
+	if(db) {
+		next();
+	}
+});
+
+router.route('/:name')
+	.get((req, res) => {
 	    const name = req.params.name.toLowerCase();
 	    const query = { 'name': name };
 	    db.collection('vegetables').findOne(query, (err, item) => {
@@ -10,9 +22,37 @@ module.exports = function(app, db) {
 	        	res.send(item);
 	      	} 
 	    });
+  	})
+
+  	.delete((req, res) => {
+    	const name = req.params.name.toLowerCase();
+	    const query = { 'name': name };
+    	db.collection('vegetables').remove(query, (err, item) => {
+      		if (err) {
+        		res.send({'error':'An error has occurred'});
+      		} else {
+        		res.send('Vegetable ' + name + ' deleted!');
+      		} 
+    	});
+  	})
+
+  	.put(parseUrlencoded, (req, res) => {
+	    const name = req.params.name.toLowerCase();
+	    const query = { 'name': name };
+	    console.log(query);
+	    const vegetable = { name: req.body.name.toLowerCase(), price: parseFloat(req.body.price) };
+	    console.log(vegetable);
+	    db.collection('vegetables').update(query, vegetable, (err, result) => {
+	      	if (err) {
+	          	res.send({'error':'An error has occurred'});
+	      	} else {
+	          	res.send(vegetable);
+	      	} 
+	    });
   	});
 
-	app.post('/vegetables', (req, res) => {
+router.route('/')
+	.post(parseUrlencoded, (req, res) => {
     	const vegetable = { name: req.body.name.toLowerCase(), price: parseFloat(req.body.price) };
     	db.collection('vegetables').insert(vegetable, (err, result) => {
 	      	if (err) { 
@@ -23,29 +63,4 @@ module.exports = function(app, db) {
     	});
   	});
 
-  	app.delete('/vegetables/:name', (req, res) => {
-    	const name = req.params.name.toLowerCase();
-	    const query = { 'name': name };
-    	db.collection('vegetables').remove(query, (err, item) => {
-      		if (err) {
-        		res.send({'error':'An error has occurred'});
-      		} else {
-        		res.send('Vegetable ' + name + ' deleted!');
-      		} 
-    	});
-  	});
-
-  	app.put ('/vegetables/:name', (req, res) => {
-	    const name = req.params.name.toLowerCase();
-	    const query = { 'name': name };
-	    const vegetable = { name: req.body.name.toLowerCase(), price: parseFloat(req.body.price) };
-	    db.collection('vegetables').update(query, vegetable, (err, result) => {
-	      	if (err) {
-	          	res.send({'error':'An error has occurred'});
-	      	} else {
-	          	res.send(vegetable);
-	      	} 
-	    });
-  	});
-
-};
+module.exports = router;
--- server.js
+++ server.js
@@ -1,21 +1,19 @@
 const express = require('express'),
       MongoClient = require('mongodb').MongoClient,
-      bodyParser = require('body-parser'),
       db = require('./config/db'),
       app = express(),
-      port = 8080;
-
-app.use(bodyParser.urlencoded({ extended: false }));
+      port = 8080,
+      vegetables = require('./app/routes/vegetablesRoutes');
 
 MongoClient.connect(db.url, (err, database) => {
   if (err) {
   	return console.log(err);
   }
 
-  require('./app/routes')(app, database);
+  app.set('db', database);
+  app.use('/vegetables', vegetables);
   
   app.listen(port, () => {
     console.log('We are live on ' + port);
   });               
-})
-
+});
